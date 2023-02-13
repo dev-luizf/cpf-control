12b1b9bcf320aa7b9477010af2e38786568f65c9
dev-luizf <luizfurtadomkt@hotmail.com>
2023-02-13 18:34:35 -0300

    feat: implementa util de validaão

---
+++ src/utils/joiValidate.ts
@@ -0,0 +1,18 @@
+import { BadRequestException } from '@nestjs/common';
+import * as Joi from 'joi';
+
+export const joiValidate = (
+  schema: Joi.ObjectSchema | Joi.ArraySchema,
+  payload: any,
+) => {
+  const { error } = schema.validate(payload);
+  
+  if (error) {    
+    const { key } = error.details[0].context;
+    let exceptionKey = key[0].toUpperCase() + key.slice(1);
+    throw new BadRequestException({
+      type: `Invalid${exceptionKey}Exception`,
+      message: error.message,
+    });
+  }
+};
