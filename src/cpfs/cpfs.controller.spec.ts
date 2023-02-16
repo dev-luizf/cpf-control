0d511f55a003711ddfc66430a6fad978892830a5
dev-luizf <luizfurtadomkt@hotmail.com>
2023-02-16 15:37:34 -0300

    test: implementa testes unitários para o cpf controller e cpf service

--- src/cpfs/cpfs.controller.spec.ts
+++ src/cpfs/cpfs.controller.spec.ts
@@ -1,20 +1,110 @@
+import { BadRequestException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { CpfsController } from './cpfs.controller';
 import { CpfsService } from './cpfs.service';
+import { AddCpfDto } from './dto/create-cpf.dto';
+
+const cpfs = [
+  {
+    cpf: '11111111111',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    cpf: '11111111112',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
+
+const newCpf = { cpf: '35139532085' };
+const expectedError = { type: 'InvalidCpfException', message: 'CPF is not valid.' };
 
 describe('CpfsController', () => {
   let controller: CpfsController;
+  let service: CpfsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CpfsController],
-      providers: [CpfsService],
+      providers: [
+        {
+          provide: CpfsService,
+          useValue: {
+            findAll: jest.fn().mockResolvedValue(cpfs),
+            findOne: jest.fn().mockImplementation((cpf: string) =>
+              Promise.resolve(cpfs[0]),
+            ),
+            add: jest
+              .fn()
+              .mockImplementation((cpf: AddCpfDto) =>
+                Promise.resolve({ cpf: '11111111111' }),
+              ),
+            remove: jest.fn().mockResolvedValue({}),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<CpfsController>(CpfsController);
+    service = module.get<CpfsService>(CpfsService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('findAll', () => {
+    it('deve retornar uma lista de CPFs', async () => {
+      jest.spyOn(service, 'findAll').mockResolvedValue(cpfs);
+
+      expect(await controller.findAll()).toEqual(cpfs);
+    });
+  });
+
+  describe('findOne', () => {
+    it('deve retornar o CPF especificado', async () => {
+      jest.spyOn(service, 'findOne').mockResolvedValue(cpfs[0]);
+
+      expect(await controller.findOne('35139532085')).toEqual(cpfs[0]);
+    });
+
+    it('deve retornar um erro se o CPF não for valido', async () => {
+      try {
+        await controller.findOne('11111111111');
+      } catch (error) {
+        expect(error).toBeInstanceOf(BadRequestException);
+        expect(error.response).toEqual(expectedError);
+      }
+    });
+  });
+
+  describe('create', () => {
+    it('deve criar um novo CPF', async () => {
+      jest.spyOn(service, 'add').mockResolvedValue(cpfs[0]);
+      expect(await controller.create(newCpf)).toEqual(cpfs[0]);
+    });
+
+    it('deve retornar um erro se o CPF não for valido', async () => {
+      try {
+        await controller.findOne('11111111111');
+      } catch (error) {
+        expect(error).toBeInstanceOf(BadRequestException);
+        expect(error.response).toEqual(expectedError);
+      }
+    });
+  });
+
+  describe('delete', () => {
+    it('deve excluir um CPF existente', async () => {
+      jest.spyOn(service, 'remove').mockResolvedValue();
+
+      await expect(controller.remove('35139532085')).resolves.not.toThrow();
+    });
+
+    it('deve retornar um erro se o CPF não for valido', async () => {
+      try {
+        await controller.findOne('11111111111');
+      } catch (error) {
+        expect(error).toBeInstanceOf(BadRequestException);
+        expect(error.response).toEqual(expectedError);
+      }
+    });
   });
 });
--- src/cpfs/cpfs.service.spec.ts
+++ src/cpfs/cpfs.service.spec.ts
@@ -1,18 +1,114 @@
-import { Test, TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
 import { CpfsService } from './cpfs.service';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AddCpfDto } from './dto/create-cpf.dto';
+import { ConflictException, NotFoundException } from '@nestjs/common';
+
+const cpfsList = [
+  {
+    cpf: '11111111111',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+  {
+    cpf: '11111111112',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
+];
+const cpf = '11111111111';
+const newCpf = { cpf };
+const conflictError = { type: 'ExistsCpfException', message: 'CPF already exists.' };
+const notFounError = { type: 'NotFoundCpfException', message: 'CPF not found.' };
 
 describe('CpfsService', () => {
-  let service: CpfsService;
+  let cpfsService: CpfsService;
+  let prisma: PrismaService;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      providers: [CpfsService],
+    const moduleRef = await Test.createTestingModule({
+      providers: [
+        CpfsService, 
+        {
+          provide: PrismaService,
+          useValue: {
+            cpf: {
+              findMany: jest.fn().mockResolvedValue(cpfsList),
+              findUnique: jest.fn().mockImplementation((cpf: string) =>
+                Promise.resolve(cpfsList[0]),
+              ),
+              create: jest
+                .fn()
+                .mockImplementation((cpf: AddCpfDto) =>
+                  Promise.resolve(cpfsList[0]),
+                ),
+              delete: jest.fn().mockResolvedValue({}),
+            }
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<CpfsService>(CpfsService);
+    cpfsService = moduleRef.get<CpfsService>(CpfsService);
+    prisma = moduleRef.get<PrismaService>(PrismaService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  describe('add', () => {
+    it('deve retornar um novo cpf', async () => {
+      jest.spyOn(prisma.cpf, 'findUnique').mockResolvedValue(null);
+      const createdCpf = await cpfsService.add(newCpf);
+      expect(createdCpf.cpf).toEqual(cpf);
+    });
+
+    it('deve retornar um erro se o CPF já existe', async () => {
+      try {
+        await cpfsService.add(newCpf);
+      } catch (error) {
+        expect(error).toBeInstanceOf(ConflictException);
+        expect(error.response).toEqual(conflictError);
+      }
+    });
+  });
+
+  describe('findAll', () => {
+    it('deve retornar uma lista de cpfs', async () => {
+      const cpfs = await cpfsService.findAll();
+      expect(Array.isArray(cpfs)).toBe(true);
+    });
+  });
+
+  describe('findOne', () => {
+    it('deve retornar um cpf se existir', async () => {
+      const result = await cpfsService.findOne(cpf);
+      expect(result.cpf).toEqual(newCpf.cpf);
+    });
+
+    it('deve retornar um erro se o CPF não for encontrado', async () => {
+      jest.spyOn(prisma.cpf, 'findUnique').mockResolvedValue(null);
+
+      try {
+        await cpfsService.findOne(cpf);
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+        expect(error.response).toEqual(notFounError);
+      }
+    });
+  });
+
+  describe('remove', () => {
+    it('deve remover um cpf se existir', async () => {
+      await expect(cpfsService.remove(cpf)).resolves.not.toThrow();
+    });
+
+    it('deve retornar um erro se o CPF não for encontrado', async () => {
+      jest.spyOn(prisma.cpf, 'findUnique').mockResolvedValue(null);
+
+      try {
+        await cpfsService.remove(cpf);
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+        expect(error.response).toEqual(notFounError);
+      }
+    });
   });
 });
